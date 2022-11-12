58c8c93370503acc90adde2b8f2f038289906604
JeanShain <112819277+JeanShain@users.noreply.github.com>
2022-11-12 16:26:41 +0200

    Add files via upload

--- project_1/script/script.js
+++ project_1/script/script.js
@@ -4,6 +4,20 @@ $('.about-items > div').on('click', function(){
     $(this).next('.about-text').slideToggle(300);
 })
 
+$('.s-4_info > div').on('click', function(){
+    $(this).children('.s-4_title').toggleClass('s-4-color-title');
+    $(this).parent('.s-4_info').toggleClass('s-4-color');
+    $(this).next('.s-4_text').slideToggle(300);
+})
+
+
+
+
+
+
+
+
+
 $('#about-link').on('click', function(){
     $('html, body').animate({scrollTop: $('#about').offset().top}, 1000)
 })
